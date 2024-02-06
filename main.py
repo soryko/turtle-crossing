89965f9976616891a9e84d98fce9f71f1bfbefea
soryko <sorykora@gmail.com>
2024-02-06 11:20:25 +0100

    added player object and added position argument to player

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import time 
 from turtle import Screen
 from player import Player
-from car_manager import CarMmanager
-from scoreboard import Scoreboard
+from car_manager import CarManager
+#from scoreboard import Scoreboard
 
+#create screen
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(width=600, height=600)
 screen.title("Turtle Crossing")
 screen.tracer(0)
 
+#moving objects
+player = Player((0,0))
+cars = CarManager()
+
+
+#refresh screen
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
--- player.py
+++ player.py
@@ -1,10 +1,12 @@
 from turtle import Turtle
 
 class Player(Turtle):
-    def __init__(self):
+
+    def __init__(self, position):
         super().__init__()
         self.color("white")
         self.penup()
+        self.goto(position)
     
     def go_up(self):
         new_y = self.ycor()
